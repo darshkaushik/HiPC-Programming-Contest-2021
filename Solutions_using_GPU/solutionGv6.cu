5066b486d0c658863ddc172ed6180b2039d0f56e
darshkaushik <darsh.kaushik@gmail.com>
2021-10-22 17:11:01 +0530

    parallelizing adjacency matrix
    cannot use binary search

--- Solutions_using_GPU/solutionGv6.cu
+++ Solutions_using_GPU/solutionGv6.cu
@@ -1,10 +1,11 @@
 // Finding number of K-Cliques in an undirected graph
-// Only parallelizing v6 for degree and v_size
+// Only parallelizing v6 for degree and adjacency matrix
 
 #include <iostream>
 #include <algorithm>
 #include <map>
 #include <chrono>
+#include <assert.h>
 
 // error checking macro
 #define cudaCheckErrors(msg) \
@@ -23,13 +24,16 @@ using namespace std;
 using namespace std::chrono;
 
 // It will store graph like adjacency list.
-int **v;
+int *v;
 int *v_size;
 int n, m, k, cnt;
 
 // It will recurse and find all possible K-Cliques and increment cnt if a K-Clique is found.
 void find(int i, int options[], int options_size)
 {
+    //for(int i1 = 0; i1 < options_size; i1++)
+    //    cout<<options[i1]<<" ";
+    //cout<<endl;
     if(k-i+1 > options_size) return;
     if(i == k)
     {
@@ -42,26 +46,40 @@ void find(int i, int options[], int options_size)
         int x = options[i1];
         
         // Finding intersection of options and v[x]
-        int intersec_size = 0;
-        for(int i2 = 0; i2 < v_size[x]; i2++)
+        int intersec_size = 0, vsz = v_size[x] - v_size[x-1];
+        for(int i2 = 0; i2 < vsz; i2++)
         {
-            int nd = v[x][i2];
-            if(binary_search(options, options + options_size, nd))
-                intersec_size++;
+            int nd = v[v_size[x-1]+i2];
+            for(int i3=0;i3<options_size;i3++)
+            {
+                if(options[i3]==nd)
+                    intersec_size++;
+            }
+            //if(binary_search(options, options + options_size, nd))
+            //    intersec_size++;
         }
         
         int *intersec = (int*) malloc(intersec_size * sizeof(int));
-        for(int i2 = 0, j = 0; i2 < v_size[x]; i2++)
+        for(int i2 = 0, j = 0; i2 < vsz; i2++)
         {
-            int nd = v[x][i2];
-            if(binary_search(options, options + options_size, nd))
+            int nd = v[v_size[x-1]+i2];
+            for(int i3=0;i3<options_size;i3++)
             {
-                intersec[j] = nd;
-                j++;
+                if(options[i3]==nd)
+                {
+                    intersec[j] = nd;
+                    j++;
+                }
             }
+            //if(binary_search(options, options + options_size, nd))
+            //{
+            //    intersec[j] = nd;
+            //    j++;
+            //}
         }
 
         // Recursion
+        //cout<<x<<endl;
         find(i+1, intersec, intersec_size);
     }
 }
@@ -85,11 +103,36 @@ __global__ void degree(int *e1, int *e2, int *d, int *v_size, int m)
     
 }
 
+__global__ void prefix_sum(int *v_size, int n)
+{
+    for(int i=1;i<n;i++)
+    {
+        v_size[i]+=v_size[i-1];
+    }
+}
+
+__global__ void adj(int *e1, int *e2, int *v, int *v_i, int *v_size, int m)
+{
+    int idx = threadIdx.x+blockDim.x*blockIdx.x;
+    if (idx < m)
+    {
+        //printf("index = %d\n",idx);
+        int x = e1[idx], y = e2[idx];
+        // x is smaller than y
+        int i=atomicAdd(&v_i[x],1);
+        i+=v_size[x-1];
+        //printf("index = %d\n", i);
+        assert(i<m);
+        v[i]=y;
+    }
+    
+}
+
 int main()
 {
     #ifndef ONLINE_JUDGE
-    freopen("./Test_Files/input001.txt", "r", stdin);
-    freopen("output001Gv6.txt", "w", stdout);
+    freopen("./Test_Files/input103.txt", "r", stdin);
+    freopen("output103Gv6.txt", "w", stdout);
     #endif
 
 //--------------------------- INPUT Starts -----------------------------> 
@@ -110,8 +153,8 @@ int main()
     }
     n++;
     m = mp.size();
-    v = (int**) malloc(n * sizeof(int*));
-    v_size = (int*) calloc(n, sizeof(int));
+    // v = (int**) malloc(n * sizeof(int*));
+    // v_size = (int*) calloc(n, sizeof(int));
 
     // Storing unique edges in e1[i] - e2[i] 
     int *e1 = (int*) malloc(m * sizeof(int));
@@ -144,21 +187,6 @@ int main()
     // Start Time
     auto start_time = high_resolution_clock::now();
 
-    // Print this to know the number of nodes and unique edges.
-    // printf("%d %d", n, m);
-
-    // d[i] will tell degree of node i.
-    // int *d = (int*) calloc(n, sizeof(int));
-    // for(auto it: mp)
-    // {
-    //     pair<int,int> p = it.first;
-    //     int x = p.first, y = p.second;
-    //     d[x]++;
-    //     d[y]++;
-    //     // x is smaller than y
-    //     v_size[x]++;
-    // }
-
     // degree of nodes in device
     int *d_d, *d_v_size;
     cudaMalloc(&d_d, n*sizeof(int));
@@ -172,9 +200,12 @@ int main()
     //cout<< "kernel config "<<(m+deg_block_sz-1)/deg_block_sz << deg_block_sz<<endl;
     degree<<<(m+deg_block_sz-1)/deg_block_sz, deg_block_sz>>>(d_e1, d_e2, d_d, d_v_size, m);
     cudaCheckErrors("Kernel degree launch failure");
+    prefix_sum<<<1,1>>>(d_v_size, n);
+    cudaCheckErrors("Kernel prefix_sum launch failure");
     // cudaDeviceSynchronize();
-    
+
     int d[n];
+    v_size = (int*) malloc(n * sizeof(int));
     cudaMemcpy(d, d_d, n*sizeof(int), cudaMemcpyDeviceToHost);
     cudaMemcpy(v_size, d_v_size, n*sizeof(int), cudaMemcpyDeviceToHost);
     cudaCheckErrors("cudaMemcpy degree failure");
@@ -183,18 +214,33 @@ int main()
     //    cout<<i<<" "<< d[i]<<endl;
 
     // Finding adjacency list v[] of graph
-    for(int i = 0; i < n; i++)
-        v[i] = (int*)malloc(v_size[i] * sizeof(int));
+    // for(int i = 0; i < n; i++)
+    //     v[i] = (int*)malloc(v_size[i] * sizeof(int));
 
-    int *v_i = (int*)calloc(n, sizeof(int));
-    for(auto it: mp)
-    {
-        pair<int,int> p = it.first;
-        int x = p.first, y = p.second;
-        // x is smaller than y
-        v[x][v_i[x]] = y;
-        v_i[x]++;
-    }
+    // int *v_i = (int*)calloc(n, sizeof(int));
+    // for(auto it: mp)
+    // {
+    //     pair<int,int> p = it.first;
+    //     int x = p.first, y = p.second;
+    //     // x is smaller than y
+    //     v[x][v_i[x]] = y;
+    //     v_i[x]++;
+    // }
+
+    int *d_v, *d_v_i;
+    cudaMalloc(&d_v, m*sizeof(int));
+    cudaMalloc(&d_v_i, n*sizeof(int));
+    cudaCheckErrors("cudaMalloc adjacency_matrix failure");
+    cudaMemset(d_v_i, 0, n*sizeof(int));
+    cudaCheckErrors("cudaMemset adjacency_matrix failure");
+
+    adj<<<(m+deg_block_sz-1)/deg_block_sz, deg_block_sz>>>(d_e1, d_e2, d_v, d_v_i, d_v_size, m);
+    cudaCheckErrors("Kernel adjacency_matrix launch failure");
+
+    v = (int*) malloc(m * sizeof(int));
+    cudaMemcpy(v, d_v, m*sizeof(int), cudaMemcpyDeviceToHost);
+    cudaCheckErrors("cudaMemcpy adjacency_matrix failure");
+
 
     // Only those nodes will form k-clique that have degree >= k-1.
     int imp_size = 0;
@@ -213,6 +259,9 @@ int main()
             j++;
         }
     }
+ 
+    //for(int i = 0; i < imp_size; i++)
+    //    cout<<i<<" "<< imp[i]<<endl;
     
     cnt=0;
     find(1, imp, imp_size);
